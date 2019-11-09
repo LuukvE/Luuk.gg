c6d5acc70b490bf9775c5d828b89f2939833adc8
Luuk van Egeraat <l.vanegeraat@gmail.com>
2019-11-10 00:36:24 +0100

    Update landing

--- src/screens/Landing.js
+++ src/screens/Landing.js
@@ -16,8 +16,57 @@ const Landing = () => {
       </Heading>
 
       <Paragraph size="xxlarge" textAlign="center">
-        containing many standard forms and pages
+        containing standard forms and pages
       </Paragraph>
+
+      <pre>
+        {`
+1.1.0 future release (january 2020)
+
+- Feature: Offline application support
+  - Memory syncing with indexedDB on the client-side
+  - HTML5 manifest file
+- Feature: Deployment automation
+  - S3 AWS deploy to bucket
+  - Bugsnag upload sourcemap
+
+1.0.0 future release (december 2019)
+
+- Feature: Components
+  - Buttons
+  - Graphs
+  - Inputs
+    - Text
+    - Number
+    - Select
+    - Datalist
+    - Currency
+    - Color
+    - Range
+    - Drag and drop
+    - Date _(+ range)_
+    - HTML _(+ editable preview)_
+    - File _(+ progress bar)_
+    - Image _(+ cropping)_
+  - Tooltips
+  - Popovers
+- Feature: Pages
+  - Interface components to create, update and delete data records
+  - Lists able to filter, loading / empty states, pagination, sorting
+- Feature: Forms
+  - Bi-directional inputs for any text or file
+  - Validation and error messaging
+  - Multi-step indicators and subflows
+  - Remembers input if the browsers closes during form fill-out
+- Feature: Multilingual support
+  - Datetime, relative time or durations through moment.js
+  - Language-specific builds are made with webpack compiler
+- Feature: Single application router for multiple domains
+  - Dynamic CSS loading
+  - Dynamic JS loading
+  - A single app; no duplicate code
+- Feature: Google Analytics`}
+      </pre>
 
       <Anchor
         href="/sign-up"
@@ -28,27 +77,6 @@ const Landing = () => {
           go('/sign-up');
         }}
       />
-
-      <pre>
-        1.1.0 future release (january 2020) - Feature: Offline application
-        support - Memory syncing with `indexedDB` on the client-side - HTML5
-        manifest file - Feature: Deployment automation - S3 AWS deploy to bucket
-        - Bugsnag upload sourcemap 1.0.0 future release (december 2019) -
-        Feature: Components - Buttons - Graphs - Inputs - Text - Number - Select
-        - Datalist - Currency - Color - Range - Drag and drop - Date _(+ range)_
-        - HTML _(+ editable preview)_ - File _(+ progress bar)_ - Image _(+
-        cropping)_ - Tooltips - Popovers - Feature: Pages - Interface components
-        to create, update and delete data records - Lists able to filter,
-        loading / empty states, pagination, sorting - Feature: Forms -
-        Bi-directional inputs for any text or file - Validation and error
-        messaging - Multi-step indicators and subflows - Remembers input if the
-        browsers closes during form fill-out - Feature: Multilingual support -
-        Datetime, relative time or durations through `moment.js` -
-        Language-specific builds are made with `webpack` compiler - Feature:
-        Single application router for multiple domains - Dynamic CSS loading -
-        Dynamic JS loading - A single app; no duplicate code - Feature: Google
-        Analytics
-      </pre>
     </Box>
   );
 };
